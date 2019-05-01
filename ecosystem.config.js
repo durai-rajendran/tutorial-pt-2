28147b018cb8f8567939a7331101ab4a674ae55d
Rajendran <Duraimurugan.X.Rajendran.-ND@disney.com>
2019-05-01 12:50:40 -0400

    setting up PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	apps: [{
+		name: 'tutorial-2',
+		script: './index.js'
+	}],
+	deploy: {
+		production: {
+			user: 'ubuntu',
+			host: 'ec2-18-216-74-73.us-east-2.compute.amazonaws.com',
+			key: '~/.ssh/first-group.pem',
+			ref: 'origin/master',
+			repo: 'git@github.com:durai-rajendran/tutorial-pt-2.git',
+			path: 'C:\Users\rajed004\Desktop\aws\tutorial-pt-2',
+			'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+		}
+	}
+}
