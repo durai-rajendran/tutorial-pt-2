bf37df31e82f17370cd4b28a927fede6b31cc48e
Rajendran <Duraimurugan.X.Rajendran.-ND@disney.com>
2019-05-01 14:57:11 -0400

    commented out default route

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('Hey baba!!')
-})
+// app.get('/', (req, res) => {
+  // res.send('Hey baba!!')
+// })
 
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
